72af74d088286207b24ccb5baddc08741174a6ac
Bokerou <li2583230913@gmail.com>
2020-06-27 14:58:49 +0800

    Bokerou

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.sunnyweather
+package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sunnyweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
